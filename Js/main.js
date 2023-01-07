b3aace3f01269e55a60b563edfa380d40499fa0b
Mtab <107020397+DamenaM@users.noreply.github.com>
2023-01-07 18:13:46 +0300

    fix comment

--- Js/main.js
+++ Js/main.js
@@ -35,7 +35,8 @@ const projects = [
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     'image mobile': './image/Snapshoot Portfolio1 (1).png',
     'image desktop': './image/descktop/Snapshoot Portfolio.svg',
-    technologies: ['html', 'css', 'javascript'],
+    technologies: ['html', 'CSS', 'javaScript'],
+    mytecnoskill: ['github', 'ruby', 'bootstrap'],
     'link to live version':
           '',
     'link to source':
@@ -50,7 +51,8 @@ const projects = [
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     'image mobile': './image/Snapshoot Portfolio1 (2).png',
     'image desktop': './image/descktop/Snapshoot Portfolio02.png',
-    technologies: ['html', 'css', 'javascript'],
+    technologies: ['html', 'CSS', 'javaScript'],
+    mytecnoskill: ['github', 'ruby', 'bootstrap'],
     'link to live version':
           '',
     'link to source':
@@ -65,7 +67,8 @@ const projects = [
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     'image mobile': './image/Snapshoot Portfolio1 (3).png',
     'image desktop': './image/descktop/Snapshoot Portfolio.png',
-    technologies: ['html', 'css', 'javascript'],
+    technologies: ['html', 'CSS', 'javaScript'],
+    mytecnoskill: ['github', 'ruby', 'bootstrap'],
     'link to live version':
           '',
     'link to source':
@@ -80,7 +83,8 @@ const projects = [
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     'image mobile': './image/Snapshoot Portfolio1 (4).png',
     'image desktop': './image/descktop/Snapshoot Portfolio3.png',
-    technologies: ['html', 'css', 'javascript'],
+    technologies: ['html', 'CSS', 'javaScript'],
+    mytecnoskill: ['github', 'ruby', 'bootstrap'],
     'link to live version':
           '',
     'link to source':
@@ -88,6 +92,7 @@ const projects = [
     id: 3,
   },
 ];
+
 projects.forEach((element, k) => {
   const mywork = document.querySelector('.gridcontainer');
   const projectpopupCard = document.createElement('div');
@@ -141,9 +146,11 @@ function creatpopcard(event) {
   const newpopup = document.createElement('div');
   newpopup.classList.add('popup-displaycard');
   // document.body.append(newpopup);
+  mainheader.className = 'hide';
 
   firstcardBack.appendChild(newpopup);
-  allElement.classList.add('blur');
+
+  allElement.classList.add('newmenuback');
 
   const header = document.createElement('div');
   header.classList.add('pop-headerDisplay');
@@ -157,6 +164,9 @@ function creatpopcard(event) {
   const proimage = document.createElement('img');
   proimage.classList.add('imgPopup');
 
+  const proimagedeskpop = document.createElement('img');// new image desk
+  proimagedeskpop.classList.add('imgPopupdesc');
+
   const positionpart = document.createElement('div');
   positionpart.classList.add('position-descr-dv');
 
@@ -166,7 +176,10 @@ function creatpopcard(event) {
   const languagepart = document.createElement('div');
   languagepart.classList.add('langbtn-div');
 
-  const programmingtype = document.createElement('ul');
+  const mydataList = document.createElement('div');
+  mydataList.classList.add('langbtn-div-mydatalist');
+
+  const programmingtype = document.createElement('dv');
   programmingtype.classList.add('program1');
 
   const projectbtncontainer = document.createElement('div');
@@ -182,10 +195,17 @@ function creatpopcard(event) {
   newpopup.append(closeButton);
   newpopup.append(tecnoprogrammingtype);
   newpopup.append(proimage);
+  newpopup.append(proimagedeskpop);
+
   newpopup.append(positionpart);
   newpopup.append(prodescription);
+
   newpopup.append(languagepart);
+
+  newpopup.append(mydataList);
+
   newpopup.append(programmingtype);
+
   newpopup.append(projectbtncontainer);
 
   header.append(heading);
@@ -198,10 +218,13 @@ function creatpopcard(event) {
 
   positionpart.append(prodescription);
   positionpart.append(languagepart);
+
   positionpart.append(programmingtype);
+  positionpart.append(mydataList);
   positionpart.append(projectbtncontainer);
 
   languagepart.append(programmingtype);
+  languagepart.append(mydataList);
   languagepart.append(projectbtncontainer);
 
   const projectnewId = parseInt(event.target.id, 20);
@@ -214,6 +237,7 @@ function creatpopcard(event) {
 
   heading.textContent = projects[projectnewId].name;
   proimage.src = projects[projectnewId]['image mobile'];
+  proimagedeskpop.src = projects[projectnewId]['image desktop'];
 
   prodescription.textContent = projects[projectnewId].description;
   projectbtncontainer.classList.add('btncont-dv');
@@ -223,6 +247,13 @@ function creatpopcard(event) {
     projectLi.className = 'Program-skilhtml';
     projectLi.textContent = projects[projectnewId].technologies[i];
     programmingtype.appendChild(projectLi);
+  }
+
+  for (let i = 0; i < projects[projectnewId].technologies.length; i += 1) {
+    const mydataprojectLi = document.createElement('ul');
+    mydataprojectLi.className = 'Program-skilhtm-deskpop';
+    mydataprojectLi.textContent = projects[projectnewId].mytecnoskill[i];
+    mydataList.appendChild(mydataprojectLi);
   }
 
   btnseelive.href = projects[projectnewId]['link to live version'];
@@ -235,7 +266,11 @@ function creatpopcard(event) {
   btnseesource.textContent = 'See Source';
 
   function closeButtonFunction() {
+    allElement.classList.remove('newmenuback');
     newpopup.remove();
+    if (mainheader.className === 'hide') {
+      mainheader.className = 'header';
+    }
   }
   const clickCloseButton = document.getElementById('close-button');
   clickCloseButton.addEventListener('click', closeButtonFunction);
