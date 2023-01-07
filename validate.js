48c4c7308bf2c3eaa56fea727f1365a327e6a7b0
Mtab <107020397+DamenaM@users.noreply.github.com>
2023-01-07 20:57:29 +0300

    fix linter

--- validate.js
+++ validate.js
@@ -3,11 +3,11 @@ const email = document.getElementById('email');
 const error = document.getElementById('error-show');
 
 form.addEventListener('submit', (event) => {
-    const messageDiplay = [];
-    if (email.value.match(/[A-Z]/)) {
-        messageDiplay.push('Please, Write Your Email In Lowercase letter .');
-        error.innerHTML = messageDiplay.join(', ');
-        event.preventDefault();
-        event.stopPropagation();
-    }
+  const messageDiplay = [];
+  if (email.value.match(/[A-Z]/)) {
+    messageDiplay.push('Please, Write Your Email In Lowercase letter .');
+    error.innerHTML = messageDiplay.join(', ');
+    event.preventDefault();
+    event.stopPropagation();
+  }
 });
